8a751e31dfacda8ef94e1f504c0bf2c8a1734e98
Vivek_Kote <133883119+vivek-9850@users.noreply.github.com>
2024-01-13 19:25:07 +0530

    Create Friend2.cpp

---
+++ Friend2.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+class Demo
+{
+    public:
+        int i;
+    private:
+        int j;
+    protected:
+        int k;
+    public:
+        Demo()
+        {
+            i = 10;
+            j = 20;
+            k = 30;
+        }
+    friend void Display();
+};
+void Display()
+{
+    Demo obj;
+    cout<<"value of i : "<<obj.i<<"\n";
+    cout<<"value of j : "<<obj.j<<"\n";
+    cout<<"value of k : "<<obj.k<<"\n";
+}
+
+int main()
+{
+    Display();
+
+    return 0;
+}
