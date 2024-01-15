ecaac4270ebb0a79a47af8014498ed62cd3701a3
Vivek_Kote <133883119+vivek-9850@users.noreply.github.com>
2024-01-15 22:44:20 +0530

    Create Virtual4.cpp

---
+++ Virtual4.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+
+class Base
+{
+    public:
+        int i,j,k;
+        virtual void Fun()  //1000
+        {
+            cout<<"Inside Base fun\n";
+        }
+        virtual void Gun()  //2000
+        {
+            cout<<"Inside Base Gun\n";
+        }
+        void Sun()  //3000
+        {
+            cout<<"Inside Base sun\n";
+        }
+        void Run()  //4000
+        {
+            cout<<"Inside Base run\n";
+        }
+};
+
+class Derived : public Base
+{
+    public:
+        int a,b;
+        virtual void Gun()  //5000
+        {
+            cout<<"Inside derived gun\n";
+        }
+        void Run()  //6000
+        {
+            cout<<"Inside derived run\n";
+        }
+        virtual void Mun()  //7000
+        {
+            cout<<"Inside derived mun\n";
+        }
+};
+
+int main()
+{
+    cout<<sizeof(Base)<<"\n";
+    cout<<sizeof(Derived)<<"\n";
+
+    Base *bp = new Derived; 
+    bp->Fun();
+    bp->Gun();
+    bp->Sun();
+    bp->Run();
+    //bp->Mun();
+
+    return 0;
+}
